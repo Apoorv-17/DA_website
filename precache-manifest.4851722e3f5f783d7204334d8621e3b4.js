488ce1ec408e1a7eda8ceb9d21a0813e56bcd1ff
Apoorv-17 <apoorv.ydv17@gmail.com>
2021-09-01 01:13:35 +0530

    Updates

---
+++ precache-manifest.4851722e3f5f783d7204334d8621e3b4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334c795e3c0bca3639868731540c9d8e",
+    "url": "/DA_website/index.html"
+  },
+  {
+    "revision": "375c78ab788c38aad3a2",
+    "url": "/DA_website/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "4cdbc479076012112f14",
+    "url": "/DA_website/static/css/main.193a3046.chunk.css"
+  },
+  {
+    "revision": "375c78ab788c38aad3a2",
+    "url": "/DA_website/static/js/2.e69004e2.chunk.js"
+  },
+  {
+    "revision": "5e7a3bbd1766925a2d65977472adeaa7",
+    "url": "/DA_website/static/js/2.e69004e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6233e3107938c4bc75cc",
+    "url": "/DA_website/static/js/3.aefe20da.chunk.js"
+  },
+  {
+    "revision": "4cdbc479076012112f14",
+    "url": "/DA_website/static/js/main.5da51be6.chunk.js"
+  },
+  {
+    "revision": "49574893de95b467b472",
+    "url": "/DA_website/static/js/runtime-main.6335009b.js"
+  },
+  {
+    "revision": "22b5573467df75ae860d43a451d43ddd",
+    "url": "/DA_website/static/media/about-bg.22b55734.jpg"
+  },
+  {
+    "revision": "64055459cc1535350261e5feb2b001d0",
+    "url": "/DA_website/static/media/about-future.64055459.jpg"
+  },
+  {
+    "revision": "269cc7c9b0c8ed3f1e621078a983e1b2",
+    "url": "/DA_website/static/media/about-goal.269cc7c9.jpg"
+  },
+  {
+    "revision": "e0842f3f6d81f1fbc17d462f0e8bf7a5",
+    "url": "/DA_website/static/media/home-bg.e0842f3f.gif"
+  },
+  {
+    "revision": "3bda585ceabcae86a826c57305d1ff39",
+    "url": "/DA_website/static/media/home-github.3bda585c.jpg"
+  },
+  {
+    "revision": "5fe15a75df71171c078aba7a9316bfbf",
+    "url": "/DA_website/static/media/home-problem.5fe15a75.jpg"
+  },
+  {
+    "revision": "5dd72b61e716eae08bc0938e7b9d38f1",
+    "url": "/DA_website/static/media/home-react.5dd72b61.jpg"
+  },
+  {
+    "revision": "ebb145405357fc50d0783bf8941a2875",
+    "url": "/DA_website/static/media/home-why.ebb14540.jpg"
+  },
+  {
+    "revision": "c3a47618554dc9dbabbf3f67497e6b84",
+    "url": "/DA_website/static/media/home-whygit.c3a47618.png"
+  },
+  {
+    "revision": "27bbe9c74de09b6ac1776419bc534cdc",
+    "url": "/DA_website/static/media/logo.27bbe9c7.png"
+  },
+  {
+    "revision": "99d6836fe7b69ee29dee8d293e58469b",
+    "url": "/DA_website/static/media/profile.99d6836f.jpg"
+  }
+]);
